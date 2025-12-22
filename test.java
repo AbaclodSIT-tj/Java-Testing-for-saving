561dd98383ab6a7d435845e5b26bed345728a740
AbaclodSIT-tj <thomsonjesseabaclod@gmail.com>
2025-12-22 18:31:26 +0800

    java OOP notes hehe

---
+++ test.java
@@ -0,0 +1,33 @@
+public class test{
+    static boolean x=true;
+    static int y=12;
+    static String name="meowy";
+        //try use "static void" method note: this should be after the class or the main class
+    static void method(){+
+        System.out.println("Welcome to the meow world");
+    }
+    public static void main(String[] args){
+        test t=new test();// test
+        //try use object with a logical
+        if(!x){
+            System.out.println("false");
+        }else {
+            System.out.println("true");
+        }
+        //trying object in a simple mention
+        System.out.println(t.y);
+        //try manipulate the class variable
+        t.y=15;
+        System.out.println(t.y);
+        test m=new test();//create new object
+        //try do simple manipulated variable
+        m.y=39;
+        System.out.println(m.y);
+        //try use string with a different obj, still works WOW!
+        System.out.println(m.name);
+        System.out.println(t.name);
+        //try use simple calling method
+        method();//outputs inside the static void main99
+    }
+
+}
