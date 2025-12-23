0d0b747b92ca742dd47b61a7187e521c77139d4d
abaclod, Thomson Jesse N. <20252273@s.ubaguio.edu>
2025-12-23 16:56:26 +0800

    .

--- test1.java
+++ test1.java
@@ -1,3 +1,9 @@
-public class test1{
-
+class test1{
+    void hello(){
+        System.out.println(hello);
+    }
+    public static void main(Stringp[] args){
+        test1 msg= new test1();
+        System.out.println("heloo");
+    }
 }
